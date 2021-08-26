bfc082010433f764bd7a9510e5c2eafab0d0afd1
Lutz <python@tastybyte.de>
2021-08-26 22:44:00 +0200

    bind button to view (kotlin)

--- app/src/main/java/com/example/excellentview/MainActivity.kt
+++ app/src/main/java/com/example/excellentview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.excellentview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.excellentview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.text = "Jalla!"
     }
 }
